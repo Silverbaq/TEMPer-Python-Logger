b0b6ac8187e5252f6ee875ccfcd44079cdd0248f
Steffen Mogensen <feber.sm@gmail.com>
2015-03-29 14:24:16 +0000

    First Check-in

---
+++ logger.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+import subprocess
+import MySQLdb
+from time import sleep
+
+temp = "empty"
+
+def ReadTempetur():
+    global temp
+    temp = subprocess.Popen("sudo temper-poll -c -q", shell=True, stdout=subprocess.PIPE).stdout.read().rstrip()
+    print "The tempeture is %s C" %(temp.rstrip())
+
+def AddToDB():
+    # Open database connection
+    db = MySQLdb.connect("localhost","temper","repmet","TEMPer")
+    
+    # prepare a cursor object using cursor() method
+    cursor = db.cursor()
+
+    # prepares the sql query
+    sql = "INSERT INTO TEMPer.Log(TempeturC,Location)VALUES('%s','Pi1')"%(temp)
+
+    try:
+
+        # execute SQL query using execute() method.
+        cursor.execute(sql)
+
+        # Commit your changes in the database
+        db.commit()
+
+        # Fetch a single row using fetchone() method.
+        # data = cursor.fetchone()
+
+        # disconnect from server
+        db.close()
+    except:
+	# Rollback in vase there is any error
+	db.rollback()
+
+def main():
+    while (True):
+        ReadTempetur()
+	AddToDB()
+	sleep(10)
+
+main()
